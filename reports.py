a358f7c73555b7272f1b8a7142b8828610cfb530
miscelo <miscelo@tutanota.com>
2022-07-16 14:30:57 +0200

    Sending data to Django server

--- reports.py
+++ reports.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+
+from reportlab.platypus import Paragraph, Spacer, SimpleDocTemplate
+from reportlab.lib.styles import getSampleStyleSheet
+
+def generate_report(file, title, add_info):
+    styles = getSampleStyleSheet()
+    report = SimpleDocTemplate(file)
+    report_title = Paragraph(title, styles['h1'])
+    report_info = Paragraph(add_info, styles['BodyText'])
+    empty_line = Spacer(1, 20)
+    report.build([report_title, empty_line, report_info, empty_line])
+
+
 # Generate Report , create pdf
 
 # Once the images and descriptions have been uploaded to the fruit store web-server,
@@ -17,17 +31,3 @@
 # 2. the report title as the title argument,
 # 3. and the file path of the PDF to be generated as the attachment argument (use ‘/tmp/processed.pdf')
 
-#!/usr/bin/env python3
-
-from reportlab.platypus import Paragraph, Spacer, Image, SimpleDocTemplate
-from reportlab.lib.styles import getSampleStyleSheet
-
-
-def generate_report(file, title, add_info):
-    styles = getSampleStyleSheet()
-    report = SimpleDocTemplate(file)
-    report_title = Paragraph(title, styles['h1'])
-    report_info = Paragraph(add_info, styles['BodyText'])
-    empty_line = Spacer(1, 20)
-
-    report.build([report_title, empty_line, report_info, empty_line])
